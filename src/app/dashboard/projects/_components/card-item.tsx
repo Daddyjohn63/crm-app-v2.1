af8d14fea97f6e8c3fd939a686d3b26440e3a0be
Daddyjohn63 <john@webandprosper.co.uk>
2025-01-16 17:22:21 +0000

    prior to cursor changes

--- src/app/dashboard/projects/_components/card-item.tsx
+++ src/app/dashboard/projects/_components/card-item.tsx
@@ -1,6 +1,7 @@
 import { Card } from '@/db/schema';
 import { CardWithList } from '@/use-cases/types';
 import { useState } from 'react';
+import { Draggable } from '@hello-pangea/dnd';
 
 interface CardItemProps {
   data: Card;
@@ -11,16 +12,23 @@ export const CardItem = ({ data, index }: CardItemProps) => {
   const [isPressed, setIsPressed] = useState(false);
 
   return (
-    <div
-      role="button"
-      onMouseDown={() => setIsPressed(true)}
-      onMouseUp={() => setIsPressed(false)}
-      onMouseLeave={() => setIsPressed(false)}
-      className={`truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm text-black rounded-md shadow-sm ${
-        isPressed ? 'bg-slate-500' : 'bg-white'
-      }`}
-    >
-      {data.name}
-    </div>
+    <Draggable draggableId={data.id.toString()} index={index}>
+      {provided => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          onMouseDown={() => setIsPressed(true)}
+          onMouseUp={() => setIsPressed(false)}
+          onMouseLeave={() => setIsPressed(false)}
+          className={`truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm text-black rounded-md shadow-sm ${
+            isPressed ? 'bg-slate-500' : 'bg-white'
+          }`}
+        >
+          {data.name}
+        </div>
+      )}
+    </Draggable>
   );
 };
--- src/app/dashboard/projects/_components/list-item.tsx
+++ src/app/dashboard/projects/_components/list-item.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { Draggable, Droppable } from '@hello-pangea/dnd';
 import { ListWithCards } from '@/use-cases/types';
 import { ListHeader } from './list-header';
 //import { ElementRef, useRef, useState } from 'react';
@@ -16,36 +16,41 @@ interface ListItemProps {
 
 export const ListItem = ({ data, index, canUseListForm }: ListItemProps) => {
   console.log('list item', data);
-  // const textareaRef = useRef<ElementRef<'textarea'>>(null);
-
-  // const [isEditing, setIsEditing] = useState(false);
-
-  // const disableEditing = () => {
-  //   setIsEditing(false);
-  // };
-  // const enableEditing = () => {
-  //   setIsEditing(true);
-  //   setTimeout(() => {
-  //     textareaRef.current?.focus();
-  //   });
-  // };
 
   return (
-    <li className="shrink-0 h-full w-[272px] select-none">
-      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md py-1">
-        <ListHeader data={data} canUseListForm={canUseListForm} />
-        <ol
-          className={cn(
-            'mx-1 px-1 py-0.5 flex flex-col gap-y-2',
-            data.cards.length > 0 ? 'mt-2' : 'mt-0'
-          )}
+    <Draggable draggableId={data.id.toString()} index={index}>
+      {provided => (
+        <li
+          className="shrink-0 h-full w-[272px] select-none"
+          {...provided.draggableProps}
+          ref={provided.innerRef}
         >
-          {data.cards.map((card, index) => (
-            <CardItem key={card.id} data={card} index={index} />
-          ))}
-        </ol>
-        <CardModal data={data} />
-      </div>
-    </li>
+          <div className="w-full rounded-md bg-[#f1f2f4] shadow-md py-1">
+            <div {...provided.dragHandleProps}>
+              <ListHeader data={data} canUseListForm={canUseListForm} />
+            </div>
+
+            <Droppable droppableId={data.id.toString()} type="card">
+              {provided => (
+                <ol
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  className={cn(
+                    'mx-1 px-1 py-0.5 flex flex-col gap-y-2',
+                    data.cards.length > 0 ? 'mt-2' : 'mt-0'
+                  )}
+                >
+                  {data.cards.map((card, index) => (
+                    <CardItem key={card.id} data={card} index={index} />
+                  ))}
+                  {provided.placeholder}
+                </ol>
+              )}
+            </Droppable>
+            <CardModal data={data} />
+          </div>
+        </li>
+      )}
+    </Draggable>
   );
 };
