9a7352a07275fd839cdbcfd2e7f1c5d16a73561b
Daddyjohn63 <john@webandprosper.co.uk>
2025-01-28 15:39:58 +0000

    more migrate fixes

--- drizzle/0004_fix_role_enum.sql
+++ drizzle/0004_fix_role_enum.sql
@@ -1,20 +1,6 @@
 DO $$ 
-DECLARE 
-    enum_exists boolean;
 BEGIN
-    SELECT EXISTS (
-        SELECT 1 FROM pg_type 
-        WHERE typname = 'role'
-    ) INTO enum_exists;
-
-    IF NOT enum_exists THEN
-        CREATE TYPE "public"."role" AS ENUM('admin', 'guest', 'member');
-    ELSE
-        BEGIN
-            ALTER TYPE "public"."role" ADD VALUE IF NOT EXISTS 'guest';
-            ALTER TYPE "public"."role" ADD VALUE IF NOT EXISTS 'member';
-        EXCEPTION WHEN OTHERS THEN
-            NULL;
-        END;
-    END IF;
+    ALTER TYPE "public"."role" ADD VALUE IF NOT EXISTS 'guest';
+EXCEPTION WHEN OTHERS THEN
+    NULL;
 END $$;
