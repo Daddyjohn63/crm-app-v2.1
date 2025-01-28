01582ad5ccbb4589c2ff61efde97e62d04dc5a39
Daddyjohn63 <john@webandprosper.co.uk>
2025-01-28 15:46:49 +0000

    migrations

--- drizzle/0003_little_turbo.sql
+++ drizzle/0003_little_turbo.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "cards" ALTER COLUMN "assigned_to" SET DATA TYPE serial;--> statement-breakpoint
+ALTER TABLE "cards" ALTER COLUMN "assigned_to" SET DATA TYPE integer;
 ALTER TABLE "cards" ALTER COLUMN "assigned_to" SET NOT NULL;
