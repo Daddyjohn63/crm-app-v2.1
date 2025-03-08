d5e1d4dcb4db9bc6f63ce5748adc602d69a7fe6d
Daddyjohn63 <john@webandprosper.co.uk>
2025-03-08 15:31:17 +0000

    fixed profile image hopefully

--- src/app/dashboard/settings/profile/_components/profile-image.tsx
+++ src/app/dashboard/settings/profile/_components/profile-image.tsx
@@ -1,7 +1,10 @@
 import { Profile } from '@/db/schema/index';
 import { ProfileImageForm } from './profile-image-form';
 import { getCurrentUser } from '@/lib/session';
-import { getProfileImageUrl } from '@/use-cases/users';
+import {
+  getProfileImageUrl,
+  getProfileImageUrlUseCase
+} from '@/use-cases/users';
 import Image from 'next/image';
 import { ConfigurationPanel } from '@/components/configuration-panel';
 import { Skeleton } from '@/components/ui/skeleton';
@@ -9,12 +12,14 @@ import { Suspense } from 'react';
 import { getUserProfileLoader } from '../page';
 //import { getUserProfileLoader } from './page';
 
-export function getProfileImageFullUrl(profile: Profile) {
-  return profile.imageId
-    ? getProfileImageUrl(profile.userId, profile.imageId)
-    : profile.image
-    ? profile.image
-    : '/profile.png';
+export async function getProfileImageFullUrl(profile: Profile) {
+  if (profile.imageId) {
+    return await getProfileImageUrlUseCase({
+      userId: profile.userId,
+      imageId: profile.imageId
+    });
+  }
+  return profile.image ?? '/profile.png';
 }
 
 export async function ProfileImage() {
@@ -29,19 +34,20 @@ export async function ProfileImage() {
 
 async function ProfileImageContent() {
   const user = await getCurrentUser();
+  //console.log('user from profile image', user);
 
   if (!user) {
     return null;
   }
 
   const profile = await getUserProfileLoader(user.id);
+  const imageUrl = await getProfileImageFullUrl(profile);
 
   return (
     <div className="flex flex-col sm:items-center">
       <Image
-        //src={getProfileImageFullUrl(profile)}
+        src={imageUrl}
         width={200}
-        src="https://crm-buddy-database-next-js14-crm-buddy.fbhmyq.easypanel.host/api/users/1/images/e9d7901e-6ec3-4ff8-bf27-0c05288fd91b"
         height={200}
         className="h-[200px] sm:h-[200px] w-full object-cover rounded-xl mb-4 sm:mb-6"
         alt="Profile image"
